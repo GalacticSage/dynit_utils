f10ead0a6c19a1cb1cc3d5a525fd3eeb31dc4862
Johann Senoner <senoner.johann@proton.me>
2024-04-14 19:13:24 +0200

    FMAB QoL improvements

---
+++ Fullmetal Alchemist Brotherhood/BD/all_discs.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./disc_1.sh
+./disc_2.sh
+./disc_3.sh
+./disc_4.sh
+./disc_5.sh
+./disc_6.sh
+./disc_7.sh
+./disc_8.sh
---
+++ Fullmetal Alchemist Brotherhood/BD/all_oavs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+./disc_1_oav.sh
+./disc_3_oav.sh
+./disc_5_oav.sh
+./disc_7_oav.sh
